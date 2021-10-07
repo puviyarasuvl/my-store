10163c2bf6355a53b3c2f5b00a3b320e7fe5f4f8
Puviyarasu V <puviyarasuvelu@gmail.com>
2021-10-07 22:21:48 +0530

    Updated CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 const port = 3000;
 
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_ORIGIN_URL }));
+app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
