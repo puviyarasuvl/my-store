0c6050161a0c02e14613e2ca74df12c08cc03367
Puviyarasu V <puviyarasuvelu@gmail.com>
2021-10-11 12:32:35 +0530

    Updated the prod server details

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
+import auth from '../../auth_config_prod.json';
+
+let domain = auth.domain;
+let clientId = auth.clientId;
+let audience = auth.audience;
+let serverUrl = auth.serverUrl;
+
 export const environment = {
-  production: true
+    production: true,
+    auth: {
+        domain,
+        clientId,
+        redirectUri: window.location.origin,
+        audience,
+    },
+    dev: {
+        serverUrl,
+    },
 };
