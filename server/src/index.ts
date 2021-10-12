79bed5ddc2200dcd8c6f6448806affaf9abe442b
Puviyarasu V <puviyarasuvelu@gmail.com>
2021-10-12 23:20:40 +0530

    Added info message to root route

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,6 +19,12 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (_req, res) => {
+    res.send(
+        'Server is up. API is ready for use. Please access the correct endpoints.'
+    );
+});
+
 app.use('/api', routes);
 
 console.log('Port ', process.env.PORT);
